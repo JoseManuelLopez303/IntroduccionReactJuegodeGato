1d7ec9d4449356b7d58da2e03b6c7652ffed7055
JoseManuelLopez303 <hermanoslopez539@gmail.com>
2022-05-05 23:59:27 -0600

    Agregando key a render de metodo game

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -74,13 +74,14 @@ class Game extends React.Component {
     const history = this.state.history;
     const current = history[history.length - 1];
     const winner = calculateWinner(current.squares);
-    
+
     const moves = history.map((step, move) => {
       const desc = move ?
         'Go to move #' + move :
         'Go to game start';
       return (
-        <li>
+
+        <li key={move}>
           <button onClick={() => this.jumpTo(move)}>{desc}</button>
         </li>
       );
@@ -103,7 +104,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
