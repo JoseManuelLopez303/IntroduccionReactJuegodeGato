e0e1de11d4d2763870aad16e6fda07feef8a7013
JoseManuelLopez303 <hermanoslopez539@gmail.com>
2022-05-05 22:51:37 -0600

    Square llame a la funcion cuando el cuadro sea clickeado

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -31,8 +31,13 @@ class Board extends React.Component {
       squares: Array(9).fill(null),
     };
   }
-  renderSquare(i) {
-    return <Square value={this.state.squares[i]} />;
+  renderSquare(i) { 
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
